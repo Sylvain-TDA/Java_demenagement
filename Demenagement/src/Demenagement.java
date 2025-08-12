353ad69dfe8a9727187e6518342282a2c93cb9fd
Sylvain <sylvain.teissier@le-campus-numerique.fr>
2025-08-12 16:12:25 +0200

    refactorisation du code

--- Demenagement/src/Demenagement.java
+++ Demenagement/src/Demenagement.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Demenagement {
     /**
      * The main method calculate how many boxes will be carried during a moving transport. It depends on the number of boxes to move and the truck capacity.
+     *
      * @param args : not use here
      */
 
@@ -22,20 +23,20 @@ public class Demenagement {
     /**
      * The method request the user to enter an integer.
      * It loops until the user enter a valid value.
+     *
      * @param keyboard is the input by the user
-     * @param message is the message display depending on the value requested.
+     * @param message  is the message display depending on the value requested.
      * @return the integer greater than 0 entered by the user.
      */
 
     public static int askInteger(Scanner keyboard, String message) {
-        String input;
+        int input;
         while (true) {
             System.out.println(message);
-            input = keyboard.nextLine();
-            if (isInteger(input)) {
-                int validatedInteger = Integer.parseInt(input);
-                if (validatedInteger >= 0) {
-                    return validatedInteger;
+            if (keyboard.hasNextInt()) {
+                input = keyboard.nextInt();
+                if (input > 0) {
+                    return input;
                 } else {
                     System.out.println("Erreur : veuillez entrer un nombre positif");
                 }
@@ -44,22 +45,6 @@ public class Demenagement {
             }
         }
     }
-
-
-    /**
-     * The method test if the number entered by the user is an integer.
-     * @param input is the input by the user
-     * @return {@code true} if the value is an integer {@code false} if not.
-     */
-
-public static boolean isInteger(String input) {
-    try {
-        Integer.parseInt(input);
-        return true;
-    } catch (NumberFormatException e) {
-        return false;
-    }
-}
 }
 
 
