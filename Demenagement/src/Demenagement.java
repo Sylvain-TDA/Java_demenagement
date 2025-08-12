8f1428072bb5d8e92d5fa88d65e4535a6467e3f6
Sylvain <sylvain.teissier@le-campus-numerique.fr>
2025-08-12 14:31:00 +0200

    correction du code

--- Demenagement/src/Demenagement.java
+++ Demenagement/src/Demenagement.java
@@ -1,50 +1,66 @@
 import java.util.Scanner;
 
 public class Demenagement {
+    /**
+     * The main method calculate how many boxes will be carried during a moving transport. It depends on the number of boxes to move and the truck capacity.
+     * @param args : not use here
+     */
+
     public static void main(String[] args) {
-        int cartons = 0;
-        int truckCapacity= 0;
-        int nbrCartonsParVoyage = 0;
+        Scanner keyboard = new Scanner(System.in);
 
+        int boxes = askInteger(keyboard, "Saisir votre nombre de cartons : ");
+        int truckCapacity = askInteger(keyboard, "Saisir la capacité du camion : ");
 
+        while (boxes != 0) {
+            int boxesNumberbyTravel = (boxes >= truckCapacity) ? truckCapacity : boxes;
+            boxes -= boxesNumberbyTravel;
+            System.out.println("Voyage de " + boxesNumberbyTravel + " cartons.");
+        }
+    }
 
-        Scanner clavier = new Scanner(System.in);
-        boolean inputCartonsValide = false;
+    /**
+     * The method request the user to enter an integer.
+     * It loops until the user enter a valid value.
+     * @param keyboard is the input by the user
+     * @param message is the message display depending on the value requested.
+     * @return the integer greater than 0 entered by the user.
+     */
 
-        while (!inputCartonsValide) {
-            System.out.print("Saisir votre nombre de cartons : ");
-            String input = clavier.nextLine();
-            try {
-                cartons = Integer.parseInt(input);
-                inputCartonsValide = true;
-            } catch (NumberFormatException e) {
-                System.out.println("Entrée invalide. Veuillez entrer un nombre valide.");
+    public static int askInteger(Scanner keyboard, String message) {
+        String input;
+        while (true) {
+            System.out.println(message);
+            input = keyboard.nextLine();
+            if (isInteger(input)) {
+                int integerInput = Integer.parseInt(input);
+                if (integerInput >= 0) {
+                    return integerInput;
+                } else {
+                    System.out.println("Erreur : veuillez entrer un nombre positif");
+                }
+            } else {
+                System.out.println("Erreur : veuillez entrer un nombre entier.");
             }
         }
+    }
 
-        boolean inputCapaciteValide = false;
 
-        while (!inputCapaciteValide) {
-            System.out.print("Saisir la capacité de votre camion : ");
-            String input = clavier.nextLine();
-            try {
-                truckCapacity = Integer.parseInt(input);
-                inputCapaciteValide = true;
-            } catch (NumberFormatException e) {
-                System.out.println("Entrée invalide. Veuillez entrer un nombre valide.");
-            }
-        }
+    /**
+     * The method test if the number entered by the user is an integer.
+     * @param input is the input by the user
+     * @return {@code true} if the value is an integer {@code false} if not.
+     */
 
-        while (cartons != 0){
-            if (cartons >= truckCapacity) {
-                nbrCartonsParVoyage = truckCapacity;
-            } else{
-                nbrCartonsParVoyage = cartons;
-            };
-           cartons -= nbrCartonsParVoyage;
-            System.out.println("Voyage de " + nbrCartonsParVoyage + " cartons.");
-        }
-
+public static boolean isInteger(String input) {
+    try {
+        Integer.parseInt(input);
+        return true;
+    } catch (NumberFormatException e) {
+        return false;
     }
 }
+}
+
+
 
