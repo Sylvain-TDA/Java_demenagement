a1c9a970791100222c5d2621badf5ff6c871aaa2
Sylvain <sylvain.teissier@le-campus-numerique.fr>
2025-08-12 11:53:08 +0200

    partie 6 avec entrée utilisateur

--- Demenagement/src/Demenagement.java
+++ Demenagement/src/Demenagement.java
@@ -1,12 +1,43 @@
+import java.util.Scanner;
+
 public class Demenagement {
     public static void main(String[] args) {
-        int cartons = 34;
-        int truckCapacity = 9;
+        int cartons = 0;
+        int truckCapacity= 0;
         int nbrCartonsParVoyage = 0;
+
+
+
+        Scanner clavier = new Scanner(System.in);
+        boolean inputCartonsValide = false;
+
+        while (!inputCartonsValide) {
+            System.out.print("Saisir votre nombre de cartons : ");
+            String input = clavier.nextLine();
+            try {
+                cartons = Integer.parseInt(input);
+                inputCartonsValide = true;
+            } catch (NumberFormatException e) {
+                System.out.println("Entrée invalide. Veuillez entrer un nombre valide.");
+            }
+        }
+
+        boolean inputCapaciteValide = false;
+
+        while (!inputCapaciteValide) {
+            System.out.print("Saisir la capacité de votre camion : ");
+            String input = clavier.nextLine();
+            try {
+                truckCapacity = Integer.parseInt(input);
+                inputCapaciteValide = true;
+            } catch (NumberFormatException e) {
+                System.out.println("Entrée invalide. Veuillez entrer un nombre valide.");
+            }
+        }
 
         while (cartons != 0){
             if (cartons >= truckCapacity) {
-                nbrCartonsParVoyage = 9;
+                nbrCartonsParVoyage = truckCapacity;
             } else{
                 nbrCartonsParVoyage = cartons;
             };
